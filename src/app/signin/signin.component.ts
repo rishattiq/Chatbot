ff8e1f6b95994cb5bc7fd35815262836f12969f7
rish attiq <arnishattiq@gmail.com>
2024-07-30 17:07:34 +0500

    signup page

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signin',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './signin.component.html',
-  styleUrl: './signin.component.css'
+  styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
+  email: string = '';
+  password: string = '';
 
+  onSubmit() {
+    // Implement sign-in logic here
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
 }
